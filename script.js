421e1a993348160fc7749c07f2e03d73f3a904ff
BrianCaetano <brian-caetano@hotmail.com>
2021-07-12 15:40:10 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const rgbColor = document.querySelector('#rgb-color');
 
+// Gera números aleatórios no formato RGB.
 function randomRGB() {
   rgbColor.innerHTML = `(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-}randomRGB()
+}
+randomRGB()
+
